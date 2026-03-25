83cf30a0ce5926bd101cb00513ef07762230c52e
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-25 07:32:52 +0300

    Add middleware

--- dashboard-api/index.js
+++ dashboard-api/index.js
@@ -4,8 +4,22 @@ const port = 8003;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(Date.now());
+  next();
+});
+
 app.get("/hello", (req, res) => {
   res.send("Hi");
+});
+
+app.get("/error", (req, res) => {
+  throw new Error("Error!");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500);
 });
 
 app.listen(port, () => {
