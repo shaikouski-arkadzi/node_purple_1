84587c08977040e1d3fe49515430687e3c24e20f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-04-07 06:55:16 +0300

    Add LoggerService to app like Dependency Injection

--- dashboard-api/src/app.ts
+++ dashboard-api/src/app.ts
@@ -1,15 +1,18 @@
 import express, { type Express } from "express";
 import { type Server } from "node:http";
 import { userRouter } from "./users/users.js";
+import type { LoggerService } from "./logger/logger.service.js";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8003;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -19,6 +22,6 @@ export class App {
   async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Сервер запущен на http://localhost:${this.port}`);
+    this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
   }
 }
--- dashboard-api/src/main.ts
+++ dashboard-api/src/main.ts
@@ -1,7 +1,8 @@
 import { App } from "./app.js";
+import { LoggerService } from "./logger/logger.service.js";
 
 async function main() {
-  const app = new App();
+  const app = new App(new LoggerService());
   await app.init();
 }
 
