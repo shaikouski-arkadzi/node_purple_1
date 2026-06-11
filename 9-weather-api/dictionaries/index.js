931689ac8cad18b4f63af0145bc3d33a3a2a452f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-06-11 09:08:10 +0300

    Add 9-weather-api

---
+++ 9-weather-api/dictionaries/index.js
@@ -0,0 +1,19 @@
+const messages = {
+  ru: {
+    weather: "Погода в городе",
+    temperature: "Температура",
+    feelsLike: "ощущается как",
+    humidity: "Влажность",
+    wind: "Скорость ветра",
+  },
+
+  en: {
+    weather: "Weather in",
+    temperature: "Temperature",
+    feelsLike: "feels like",
+    humidity: "Humidity",
+    wind: "Wind speed",
+  },
+};
+
+export { messages };
