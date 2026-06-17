c63e185273396cbfe11616f9dd8a1dbfaff2514f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-06-17 07:24:33 +0300

    Add more e2e tests

--- dashboard-api/tests/users.e2e-spec.ts
+++ dashboard-api/tests/users.e2e-spec.ts
@@ -11,10 +11,34 @@ beforeAll(async () => {
 
 describe("Users e2e", () => {
   it("Register - error", async () => {
-    const res = request(application.app)
+    const res = await request(application.app)
       .post("/users/register")
       .send({ email: "a@a.com", password: "123" });
-    expect((await res).statusCode).toBe(422);
+    expect(res.statusCode).toBe(422);
+  });
+
+  it("Login - success", async () => {
+    const res = await request(application.app)
+      .post("/users/login")
+      .send({ email: "a@a.com", password: "123" });
+    expect(res.body.jwt).not.toBeUndefined();
+  });
+
+  it("Login - error", async () => {
+    const res = await request(application.app)
+      .post("/users/login")
+      .send({ email: "a@a.com", password: "1" });
+    expect(res.statusCode).toBe(401);
+  });
+
+  it("Info - success", async () => {
+    const login = await request(application.app)
+      .post("/users/login")
+      .send({ email: "a@a.com", password: "123" });
+    const res = await request(application.app)
+      .get("/users/info")
+      .set("Authorization", `Bearer ${login.body.jwt}`);
+    expect(res.body.email).toBe("a@a.com");
   });
 });
 
